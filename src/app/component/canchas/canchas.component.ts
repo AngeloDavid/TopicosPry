3bd2ecf9f62f6b950b4e94c32abae3c060541f83
TatyMaybeth <tattocsb_00994@hotmail.com>
2017-05-31 21:33:35 -0500

    Inicio

--- src/app/component/canchas/canchas.component.ts
+++ src/app/component/canchas/canchas.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-canchas',
-  templateUrl: './canchas.component.html'
+  templateUrl: './canchas.component.html',
+  styleUrls: ['./canchas.component.css']
 })
 export class CanchasComponent implements OnInit {
 
