a1babb78776a7a3aba98c1e74f8903e2f60b6e55
TatyMaybeth <tattocsb_00994@hotmail.com>
2017-07-12 23:43:32 -0500

    Terminando parte canchas, campeonatos

--- AppSoccer/src/pages/campeonatos/campeonatos.ts
+++ AppSoccer/src/pages/campeonatos/campeonatos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the CampeonatosPage page.
@@ -14,11 +14,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CampeonatosPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CampeonatosPage');
   }
 
+  showConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Desea participar?',
+      message: 'Esta de acuerdo en participar con su equipo en este campeonato?',
+      buttons: [
+        {
+          text: 'Disagree',
+          handler: () => {
+            console.log('Disagree clicked');
+          }
+        },
+        {
+          text: 'Agree',
+          handler: () => {
+            console.log('Agree clicked');
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
 }
